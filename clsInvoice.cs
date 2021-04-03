799cf60f693ffe54bd5c6cf6387fbc57cc2d3922
rweston03 <randi.l.weston@gmail.com>
2021-04-02 19:22:51 -0600

    removed unnecessary list from invoice class

--- clsInvoice.cs
+++ clsInvoice.cs
@@ -23,18 +23,12 @@ namespace CS3280_GroupProject
         /// Decimal variable to store the invoice cost;
         /// </summary>
         private decimal dTotalCost;
-
-        /// <summary>
-        /// List of clsLineItems objects to store the invoice's line items.
-        /// </summary>
-        List<clsLineItem> clsLineItemsInvoice;
         #endregion
 
         #region Properties
         public int IInvoiceNum { get; set; }
         public DateTime DTInvoiceDate { get; set; }
         public decimal DTotalCost { get; set; }
-        public List<clsLineItem> CLSLineItemsInvoice { get; }
         #endregion
 
         #region Methods
